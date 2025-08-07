7b74a0f04acafb0a1bcbf54659939f96874d1bf6
jemy <mohamed555asdfgh@gmail.com>
2025-08-07 12:21:31 +0300

    changes

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -247,7 +247,7 @@ export default function Index() {
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 flex flex-col overflow-hidden lg:pl-72">
+      <div className="flex-1 flex flex-col overflow-hidden lg:pl-72 my-4">
         {/* Header */}
         <header className="bg-white border-b border-gray-200 p-4">
           <div className="flex items-center justify-between">
